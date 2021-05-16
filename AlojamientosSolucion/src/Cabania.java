fe4878c164ec13d7b11d2e4c1927aa0ad311747e
Juan <Juan@MEDION>
2021-05-16 18:32:11 +0200

    Added Class Cabania

---
+++ AlojamientosSolucion/src/Cabania.java
@@ -0,0 +1,24 @@
+
+public class Cabania extends Alojamiento{
+
+	private int superficiePorche;
+		
+	
+
+	public Cabania(String codigo, int numPlazas, int suplemento, int superficiePorche) {
+		super(codigo, numPlazas, suplemento);
+		this.superficiePorche = superficiePorche;
+	}
+
+	// En los bungalows se suma el suplemento multiplicado por el número de plazas independientemente de los días que se hayan alquilado
+	@Override
+	public double precioAPagar(int dias) {
+		
+		return getPrecioPorDia()*dias + getSuplemento()*getNumPlazas();
+	}
+	
+	@Override
+	public String toString() {
+		return super.toString() + String.format(", %-23s%5d", "Superficie del porche:", superficiePorche);
+	}
+}
